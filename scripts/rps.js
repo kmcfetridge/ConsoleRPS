f4269e696773255b3198ddc7e8459d63039a2e5c
Kenneth McFetridge <kmcfetridge89@gmail.com>
2018-05-28 15:14:23 -0700

    Polishing look

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,10 +1,13 @@
 function playRound(e) {
 	let playerChoice = convertPlayerChoiceToNum(e.target.innerHTML);
 	let computerChoice = computerPlay();
-	document.getElementById("rounds").innerHTML = round;
 	convertComputerChoiceToString(computerChoice);
 	playRPS(playerChoice, computerChoice);
 	updateScore();
+	if(isGameOver()) {
+		toggleButtons(true);
+		toggleMessageBox();
+	}
 }
 
 function getRandomInt(num) {
@@ -17,6 +20,7 @@ function computerPlay() {
 
 function playRPS(playerSelection, computerSelection ) {
 	let results = "";
+	round++;
 	switch(playerSelection) {
 		case 0:
 			switch(computerSelection) {
@@ -80,13 +84,13 @@ function convertPlayerChoiceToNum(choice) {
 function convertComputerChoiceToString(computerChoice) {
 	switch(computerChoice) {
 		case 0:
-			document.getElementById("computerChoice").innerHTML = 'Computer chose <strong>ROCK</strong>';
+			document.getElementById("computerChoice").innerHTML = 'Computer chose <strong>' + rock + '</strong>';
 			break;
 		case 1:
-			document.getElementById("computerChoice").innerHTML = 'Computer chose <strong>PAPER</strong>';
+			document.getElementById("computerChoice").innerHTML = 'Computer chose <strong>' + paper + '</strong>';
 			break;
 		case 2:
-			document.getElementById("computerChoice").innerHTML = 'Computer chose <strong>SCISSORS</strong>';
+			document.getElementById("computerChoice").innerHTML = 'Computer chose <strong>' + scissors + '</strong>';
 			break;
 	}
 }
@@ -94,4 +98,45 @@ function convertComputerChoiceToString(computerChoice) {
 function updateScore() {
 	document.getElementById("playerScore").innerHTML = playerScore;
 	document.getElementById("computerScore").innerHTML = computerScore;
+	document.getElementById("rounds").innerHTML = round;
+}
+
+function isGameOver() {
+	if(playerScore === 5 || computerScore === 5) {
+		document.getElementById("reset").innerHTML = "Play Again?";
+		if(playerScore === 5) {
+			document.getElementById("message").innerHTML = "Game Over! You Win!!!!";
+		} else {
+			document.getElementById("message").innerHTML = "Game Over! You Lose! Try again.";	
+		}
+		return true;
+	}
+	return false;
+}
+
+function toggleMessageBox() {
+	let box = document.getElementById('messageBox');
+	if (box.style.display === "none") {
+        box.style.display = "block";
+    } else {
+        box.style.display = "none";
+    }
+}
+
+function toggleButtons(disable) {
+	let buttons = document.getElementsByClassName("btn");
+	for(let i = 0;i<buttons.length;i++) {
+		buttons[i].disabled = disable;
+	}
+}
+
+function reset(e) {
+	playerScore = computerScore = round = 0;
+	updateScore();
+	document.getElementById("computerChoice").innerHTML = "";
+	document.getElementById("playerChoice").innerHTML = "";
+	document.getElementById("resultsDisplay").innerHTML = "";
+	document.getElementById("messageBox").style.animation = "slideIn 0.5s ease-out";
+	toggleMessageBox();
+	toggleButtons(false);
 }
